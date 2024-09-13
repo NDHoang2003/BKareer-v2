30f12009068e09bc0303b4e7a8100581f1fd6b9f
NDHoang2003 <hoang03fanstar@gmail.com>
2024-09-13 13:20:55 +0700

    .

--- src/screens/MBTIScreen.jsx
+++ src/screens/MBTIScreen.jsx
@@ -1,153 +1,42 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-
-import ProgressBar from "@ramonak/react-progress-bar";
+import { Link } from 'react-router-dom';
 
 import Card from "../components/Card.jsx";
 import Panel from "../components/mbtiPanel.jsx";
 import questions from "../database/MBTIQuest";
 import perlist from "../database/MBTIPersonality";
 import Result from "../database/Result.js";
+import Warning from "../database/Warning.jsx";
 
 function MBTITest() {
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleShowWarning = () => {
+    setShowWarning(true);
+  };
+
+  const handleCloseWarning = () => {
+    setShowWarning(false);
+  };
+
   const list = questions;
-  // const perlist = require("../database/MBTIPersonality");
+    // const perlist = require("../database/MBTIPersonality");
   const [per, setPer] = useState({});
   const Elist = ["1", "15", "29", "41", "57", "71", "85", "99", "113", "127"];
   const Ilist = ["2", "16", "30", "42", "58", "72", "86", "100", "114", "128"];
-  const Slist = [
-    "3",
-    "17",
-    "31",
-    "45",
-    "59",
-    "73",
-    "87",
-    "101",
-    "115",
-    "129",
-    "5",
-    "19",
-    "33",
-    "47",
-    "61",
-    "75",
-    "89",
-    "103",
-    "117",
-    "131",
-  ];
-  const Nlist = [
-    "4",
-    "18",
-    "32",
-    "46",
-    "60",
-    "74",
-    "88",
-    "102",
-    "116",
-    "130",
-    "6",
-    "20",
-    "34",
-    "48",
-    "62",
-    "76",
-    "90",
-    "104",
-    "118",
-    "132",
-  ];
-  const Tlist = [
-    "7",
-    "21",
-    "35",
-    "49",
-    "63",
-    "77",
-    "91",
-    "105",
-    "119",
-    "133",
-    "9",
-    "23",
-    "37",
-    "51",
-    "65",
-    "79",
-    "93",
-    "107",
-    "121",
-    "135",
-  ];
-  const Flist = [
-    "8",
-    "22",
-    "36",
-    "50",
-    "64",
-    "78",
-    "92",
-    "106",
-    "120",
-    "134",
-    "10",
-    "24",
-    "38",
-    "52",
-    "66",
-    "80",
-    "94",
-    "108",
-    "122",
-    "136",
-  ];
-  const Jlist = [
-    "11",
-    "25",
-    "39",
-    "53",
-    "67",
-    "81",
-    "95",
-    "109",
-    "123",
-    "137",
-    "13",
-    "27",
-    "41",
-    "55",
-    "69",
-    "83",
-    "97",
-    "111",
-    "125",
-    "139",
-  ];
-  const Plist = [
-    "12",
-    "26",
-    "40",
-    "54",
-    "68",
-    "82",
-    "96",
-    "110",
-    "124",
-    "138",
-    "14",
-    "28",
-    "42",
-    "56",
-    "70",
-    "84",
-    "98",
-    "112",
-    "126",
-    "140",
-  ];
+  const Slist = ["3", "17", "31", "45", "59", "73", "87", "101", "115", "129",
+                "5", "19", "33", "47", "61", "75", "89", "103", "117", "131"];
+  const Nlist = ["4", "18", "32", "46", "60", "74", "88", "102", "116", "130",
+                "6", "20", "34", "48", "62", "76", "90", "104", "118", "132"];
+  const Tlist = ["7", "21", "35", "49", "63", "77", "91", "105", "119", "133",
+                "9", "23", "37", "51", "65", "79", "93", "107", "121", "135"];
+  const Flist = ["8", "22", "36", "50", "64", "78", "92", "106", "120", "134", 
+                "10", "24", "38", "52", "66", "80", "94", "108", "122", "136"];
+  const Jlist = ["11", "25", "39", "53", "67", "81", "95", "109", "123", "137",
+                "13", "27", "41", "55", "69", "83", "97", "111", "125", "139"];
+    const Plist = ["12", "26", "40", "54", "68", "82", "96", "110", "124", "138",
+                  "14", "28", "42", "56", "70", "84", "98", "112", "126", "140"];
 
   const result = () => {
     let listAnswer = document.querySelectorAll('input[type="radio"]');
@@ -160,7 +49,6 @@ function MBTITest() {
     let f = 0;
     let j = 0;
     let p = 0;
-    let arr = [];
     for (let i = 0; i < listAnswer.length; i++) {
       if (listAnswer[i].checked === true) {
         count++;
@@ -188,17 +76,9 @@ function MBTITest() {
         }
       }
     }
-    for (let i = 0; i < listAnswer.length; i += 2) {
-      if (
-        listAnswer[i].checked === false &&
-        listAnswer[i + 1].checked === false
-      ) {
-        arr.push(i / 2 + 1);
-      }
-    }
     let data = "";
     if (count < 70) {
-      alert(`Bạn chưa hoàn thành bài kiểm tra ở các câu ${arr.join(", ")}`);
+      handleShowWarning();
       return;
     } else {
       if (e > ii) {
@@ -227,74 +107,27 @@ function MBTITest() {
       document.querySelector(".Panel").style.display = "flex";
 
       Result.setMbti(data);
-    }
-  };
-  const [progress, setProgress] = useState(0);
-  const scrollToNext = (index) => {
-    console.log("scrollToNext", index);
-    const nextIndex = index + 1; // Lấy index kế tiếp
-    const nextElement = document.querySelector(`div[id='div-${nextIndex}']`); // Tìm thẻ div với id chính xác
-    console.log(nextElement);
-    if (nextElement) {
-      const rect = nextElement.getBoundingClientRect(); // Lấy vị trí của thẻ div so với viewport
-      const offset = window.pageYOffset + rect.top - 280; // Tính vị trí so với toàn bộ tài liệu (document)
 
-      // Cuộn tới vị trí chính xác, loại bỏ ảnh hưởng của padding/margin
-      window.scrollTo({
-        top: offset, // Cuộn đến vị trí chính xác của thẻ div
-        behavior: "smooth",
-      });
     }
-    let count = 0;
-    let listAnswer = document.querySelectorAll('input[type="radio"]');
-    for (let i = 0; i < listAnswer.length; i += 2) {
-      if (
-        listAnswer[i].checked === true ||
-        listAnswer[i + 1].checked === true
-      ) {
-        count++;
-      }
-    }
-    setProgress(Math.floor((count / 70) * 100));
   };
+
   return (
-    <div>
-      <div className="progress-card">
-        <div className="mbti-title">Trắc nghiệm MBTI</div>
+    <>
+      <div className="body">
+        <div className="screen-title">Trắc nghiệm MBTI</div>
 
-        <ProgressBar
-          completed={progress}
-          baseBgColor="white"
-          bgColor="linear-gradient(to right, #003366, #66ccff)"
-          className="progress-bar"
-        />
-      </div>
-      <div className="body mbti-body">
-        {list.map((item, index) => (
-          <div
-            id={`div-${index}`}
-            key={item.content}
-            onClick={() => scrollToNext(index)}
-          >
-            <Card Ques={item} key={item.content} />
-          </div>
+        {list.map((item) => (
+          <Card Ques={item} key={item.content} />
         ))}
-
+        
         <div className="flex-row align-center width-fit-content">
-          <Link
-            to={`/majorResult?mbtiResult=${result}`}
-            className="primary-btn font-18 width-45 margin-right-5rem"
-          >
-            Quay về nhập kết quả
-          </Link>
-          <button className="primary-btn font-18" onClick={result}>
-            Xem kết quả
-          </button>
+          <Link to={`/majorResult?mbtiResult=${result}`} className="primary-btn font-18 width-45 margin-right-5rem">Quay về nhập kết quả</Link>
+          <button className="primary-btn font-18" onClick={result}>Xem kết quả</button>
         </div>
       </div>
-
+      {showWarning && <Warning message={"Bạn cần hoàn thành bài test trước!"}  onClose={handleCloseWarning} />}
       <Panel Data={per} />
-    </div>
+    </>
   );
 }
 
