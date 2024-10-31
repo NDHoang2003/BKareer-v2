5d1f2046540d019780d4581c62890a8fe077e5e5
NDHoang2003 <hoang03fanstar@gmail.com>
2024-11-01 02:14:37 +0700

    update lr brain test

--- src/screens/LeftRightBrainTest.jsx
+++ src/screens/LeftRightBrainTest.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ProgressBar from "@ramonak/react-progress-bar";
+import { BounceLoader } from "react-spinners";
 
 import Card from "../components/Card";
-import Panel from "../components/lrBrainPanel";
+// import Panel from "../components/lrBrainPanel";
 import questions from "../database/LrBrainQuest";
 import lrList from "../database/LrCharacteristic";
 import { useSelector } from "react-redux";
+import exit from "../assets/remove.png";
 
 export default function LeftRightBrainTest() {
   const list = questions;
   const [brain, setBrain] = useState({});
   const [loading, setLoading] = useState(true);
   const { currentUser } = useSelector((state) => state.user);
+  const [progress, setProgress] = useState(0);
+  const [score, setScore] = useState("");
+
+  const closeform = () => {
+    document.querySelector(".Panel").style.display = "none";
+    window.scrollTo(0, 0);
+  };
+
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "#04BCFC",
+    padding: "20px",
+  };
 
   const result = async () => {
     let listAnswer = document.querySelectorAll('input[type="radio"]');
@@ -19,6 +36,7 @@ export default function LeftRightBrainTest() {
     let left = 0;
     let right = 0;
     let arr = [];
+    let sum = 0;
 
     // check selected options
     for (let i = 0; i < listAnswer.length; i++) {
@@ -28,6 +46,17 @@ export default function LeftRightBrainTest() {
           left = left + 1;
         } else {
           right = right + 1;
+        }
+        for (let j = 0; j < list.length; j++) {
+          if (listAnswer[i].name === list[j].content) {
+            // Match the answerId to the selected answer
+            const selectedAnswer = list[j].answers.find(
+              (ans) => ans.answerId === listAnswer[i].id
+            );
+            if (selectedAnswer) {
+              sum += selectedAnswer.score; // Add the score of the selected answer
+            }
+          }
         }
       }
     }
@@ -49,6 +78,9 @@ export default function LeftRightBrainTest() {
       alert(`Bạn chưa hoàn thành bài kiểm tra ở các câu ${arr.join(", ")}`);
       return;
     } else {
+      setIsActive(false);
+      setScore(`${sum}/${list.length * 20}`); // Assuming 20 is max score per question
+      document.querySelector(".Panel").style.display = "flex";
       if (Math.abs(left - right) < 3) {
         data = "middle";
       } else if (left > right) {
@@ -96,11 +128,59 @@ export default function LeftRightBrainTest() {
     }
   };
 
+  const [countt, setCount] = useState(0);
+
+  const scrollToNext = () => {
+    let count = 0;
+    let listAnswer = document.querySelectorAll('input[type="radio"]');
+    for (let i = 0; i < listAnswer.length; i += 2) {
+      if (
+        listAnswer[i].checked === true ||
+        listAnswer[i + 1].checked === true 
+      ) {
+        count++;
+      }
+    }
+    setCount(count);
+    setProgress(Math.floor((count / list.length) * 100));
+  };
+
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+        const min = Math.floor(seconds / 60);
+        const sec = seconds % 60;
+        setTime(`${min}:${sec < 10 ? "0" + sec : sec}`);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval); // Cleanup interval when component unmounts
+  }, [isActive, seconds]);
+
   return (
     <>
-      <body className="body">
-        <div className="screen-title">Trắc nghiệm não trái - não phải</div>
-
+      <div className="progress-card" id="progress-card">
+        <div className="progress-card-title">Trắc nghiệm não trái - não phải</div>
+        <div className="text-bar">
+          <span>Đã hoàn thành: {countt}/{list.length} câu</span>
+          <span className="time-clock">Thời gian: {time}</span>
+        </div>
+        <ProgressBar
+          completed={progress}
+          baseBgColor="white"
+          bgColor="linear-gradient(to right, #003366, #66ccff)"
+          className="progress-bar"
+        />
+      </div>
+    
+      <body className="body shorten-top-body">
         {list.map((item, index) => (
           <div
             id={`div-${index}`}
@@ -111,12 +191,49 @@ export default function LeftRightBrainTest() {
           </div>
         ))}
 
-        <button className="primary-btn font-18 align-center" onClick={result}>
-          Xem kết quả
-        </button>
+        <div className="flex-row align-center width-fit-content">
+          <button className="primary-btn font-18" onClick={result}>
+            Xem kết quả
+          </button>
+        </div>
 
-        <Panel Data={brain} />
       </body>
+
+      <div className="Panel">
+        <BounceLoader
+          loading={loading}
+          size={150}
+          color="#50d1ff"
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          cssOverride={override}
+        />
+        {!loading && (
+          <div className="result_form margin-top-panel">
+            <img
+              src={exit}
+              alt="exit_icon"
+              width={30}
+              height={30}
+              className="img margin-top-2rem margin-right-2rem"
+              onClick={closeform}
+            />
+
+            <div className="panel_info z-50">
+              <div className="info">
+                <div className="hero hero-margin">
+                  <img src={brain.img} alt="img" width={150} height={125} className="margin-bottom-1rem"/>
+                  <span>{brain.content}</span>
+                </div>
+                <p>
+                  {brain.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
     </>
   );
 }
--- src/screens/homeComponentsDetail/LeftRightBrainDetail.jsx
+++ src/screens/homeComponentsDetail/LeftRightBrainDetail.jsx
@@ -1,9 +1,34 @@
 import { Link } from "react-router-dom";
 
+import image from "../../assets/images/left-right-brain.png"
+
 export default function LeftRightBrain() {
   return (
     <body className="body screen-block">
       <div className="screen-title">Trắc nghiệm não trái - não phải</div>
+      
+      <img src={image} className="img-detail align-center" />
+
+      <div className="detail-txt font-20 margin-top-2rem">
+      <span className="bold-txt">Trắc nghiệm Não Trái - Não Phải</span> là một phương pháp giúp khám phá xu hướng sử dụng não bộ của mỗi người, từ đó hiểu rõ hơn về đặc điểm tư duy và cách xử lý thông tin của họ. Bài trắc nghiệm này dựa trên lý thuyết về sự khác biệt giữa hai bán cầu não trái và phải, mỗi bên chịu trách nhiệm cho những chức năng và kỹ năng khác nhau.
+      </div>
+
+      <div className="detail-txt font-20">
+        Hai bán cầu não đảm nhận những chức năng khác nhau và có ảnh hưởng đến cách bạn xử lý thông tin, sáng tạo, và giao tiếp. Cụ thể:
+        <ul>
+          <li>
+            <span className="bold-txt">Não Trái:</span> Phụ trách logic, phân tích, và chi tiết. Người thiên về não trái thường giỏi xử lý thông tin một cách tuần tự và khoa học.
+          </li>
+          <li>
+            <span className="bold-txt">Não Phải:</span> Kết nối với khả năng sáng tạo, trực giác, và cảm xúc. Những người thiên về não phải thường có tư duy hình ảnh tốt, dễ đồng cảm và thích ứng với nghệ thuật.
+          </li>
+        </ul>
+      </div>
+
+      <div className="detail-txt font-20">
+        Dựa vào kết quả của bài trắc nghiệm, bạn có thể khám phá được xu hướng tư duy của mình và ứng dụng điều đó trong học tập, làm việc, cũng như các mối quan hệ hàng ngày. Hãy kiểm tra ngay để tìm hiểu về não bộ của mình!
+      </div>
+
 
       <Link to="/lrBrain" className="primary-btn font-18 align-center margin-top-2rem">Kiểm tra ngay</Link>
     </body>
